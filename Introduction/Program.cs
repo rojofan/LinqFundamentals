5c1e71f472717f68e4fec1e86f22299bb685c7b2
Carlos Gregorio <carlosgregorio85@gmail.com>
2019-10-21 17:03:17 +1300

    Create LinqFundamentals Solution and add Introduction module

---
+++ Introduction/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFileWithoutLinq(path);
+            Console.WriteLine("*********");
+            ShowLargeFileWithLinq(path);
+        }
+
+        private static void ShowLargeFileWithLinq(string path)
+        {
+            //var query = (from file in new DirectoryInfo(path).GetFiles()
+            //            orderby file.Length descending
+            //            select file);
+
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+
+            foreach (var file in query)//query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFileWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            var files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i < 5; i++)
+            {
+                var file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+
+
+        }
+
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
+        }
+    }
+}
